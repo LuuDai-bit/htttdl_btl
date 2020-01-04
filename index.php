76d01a1ba43560102da195d4f4cc237e4097f0a5
DaiLuu-bit <jojolove198@gmail.com>
2020-01-04 15:46:49 +0700

    hello

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bài tập lớn</title>
+
+    <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css" />
+    <script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
+
+    <link rel="stylesheet" href="/libs/openlayers/css/ol.css" type="text/css" />
+    <script src="/libs/openlayers/build/ol.js" type="text/javascript"></script>
+    <script src="/libs/jquery/jquery-3.4.1.min.js" type="text/javascript"></script>
+</head>
+
+<body onload="initinizeMap()">
+    <div id="map" style="width: 80vw; height: 100vh;"></div>
+    <?php
+        require_once 'pgsqlAPI.php'
+    ?>
+    <script>
+        function initinizeMap(){
+            //  this fun is empty
+        }
+    </script>
+</body>
+
+</html>
