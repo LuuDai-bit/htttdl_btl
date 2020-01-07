78f3407ed3ff0638645bd4d17e09ce647a460f15
napdoi1234 <53718542+napdoi1234@users.noreply.github.com>
2020-01-08 00:56:08 +0700

    Add files via upload
    Những chức năng chú thích ở phần ajax

--- cmr_roads_highLight.php
+++ cmr_roads_highLight.php
@@ -1,21 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>OpenStreetMap &amp; OpenLayers - Marker Example</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        
-        <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css" />
-        <script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
-        <!-- <link rel="stylesheet" href="http://localhost:8081/libs/openlayers/css/ol.css" type="text/css" />
-        <script src="http://localhost:8081/libs/openlayers/build/ol.js" type="text/javascript"></script> -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
-        <!-- <script src="http://localhost:8081/libs/jquery/jquery-3.4.1.min.js" type="text/javascript"></script> -->
-        <style>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bài tập lớn</title>
+
+    <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css" />
+    <script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
+
+    <!-- <link rel="stylesheet" href="/libs/openlayers/css/ol.css" type="text/css" />
+    <script src="/libs/openlayers/build/ol.js" type="text/javascript"></script>
+    <script src="/libs/jquery/jquery-3.4.1.min.js" type="text/javascript"></script> -->
+    <style>
             .ol-popup {
                 position: absolute;
                 background-color:#dfe1e3;
@@ -104,12 +102,12 @@
                 100% {
                     top:15px;height:100%;width:100%;
                 }
-            }
-
+            }  
         </style>
-    </head>
-    <body onload="initialize_map();" style="background-color:#dfe1e3;font-family: Serif;">
-        <table>
+</head>
+
+<body onload="initinizeMap()" style="background-color:#dfe1e3;font-family: Serif;">
+    <table>
             <tr>
                 <td>
                     <div id="map" style="width: 80vw; height: 100vh;box-shadow: 5px 5px 10px 8px #888888; " ></div>
@@ -122,7 +120,7 @@
                 <div class="layer" >
                     <div class="layer-child">
                         <div style="background-color:#37474f; color:white;font-weight: bold;text-align:center;"> Layer Options</div>  
-                        <input type="checkbox"  id="cmr_roads" checked /><label for="cmr_roads">cmr_roads</label>
+                        <input type="checkbox"  id="cmr_adm1" checked /><label for="cmr_adm1">cmr_adm1</label>
                         <input type="checkbox" id="layersOSM" checked /><label for="layersOSM">layersOSM</label>
                     </div>
                 </div>
@@ -141,126 +139,100 @@
                         </div>
                     </div>
                 </td>
-                
             </tr>
         </table>
-        <?php include 'cmr_roads_pgsqlAPI.php' ?>
-        <?php
-            //$myPDO = initDB();
-            //$mySRID = '4326';
-            //$pointFormat = 'POINT(12,5)';
-
-            //example1($myPDO);
-            //example2($myPDO);
-            //example3($myPDO,'4326','POINT(12,5)');
-            //$result = getResult($myPDO,$mySRID,$pointFormat);
-
-            //closeDB($myPDO);
-        ?>
-        <script>
-            var format = 'image/png';
-            var map;
-            var minX = 8.8262996673584;
-            var minY = 1.97410714626312;
-            var maxX = 15.5927391052246;
-            var maxY = 12.7209100723267;
-            var cenX = (minX + maxX) / 2;
-            var cenY = (minY + maxY) / 2;
-            var mapLat = cenY;
-            var mapLng = cenX;
-            var mapDefaultZoom = 7;
-            var container = document.getElementById('popup');
-            var content = document.getElementById('popup-content');
-            var closer = document.getElementById('popup-closer');
-            var overlay = new ol.Overlay({
-                element: container,
-                autoPan: true,
-                autoPanAnimation: {
-               
-                }
-            });
-            function reset(){
-                    document.getElementById("LoaderBalls__item").style.display = "block";
-                    document.getElementById("LoaderBalls__item1").style.display = "block";
-                    document.getElementById("LoaderBalls__item2").style.display = "block";
-                    document.getElementById("broad-content").innerHTML="";
-            }
-            var cmr_roads = new ol.layer.Group({
-                    layers: [ new ol.layer.Image({
-                    source: new ol.source.ImageWMS({
-                        ratio: 1,
-                        url: 'http://localhost:8080/geoserver/example/wms?',
-                        params: {
-                            'FORMAT': format,
-                            'VERSION': '1.1.1',
-                            STYLES: '',
-                            LAYERS: 'cmr_roads',
-                        }
-                    }),
-                })]
-            });
-            var layersOSM = new ol.layer.Group({
-                    layers: [
-                        new ol.layer.Tile({
-                            source: new ol.source.OSM()
-                        })
-                    ]
-            });
-            function initialize_map() {
-                closer.onclick = function() {
-                    overlay.setPosition(undefined);
-                    closer.blur();
-                    return false;
-                };
-                
-                //*
-                // layerBG = new ol.layer.Tile({
-                //     source: new ol.source.OSM({}),
-                // });
-                //*/
-                
-                var viewMap = new ol.View({
+    <?php
+        require_once 'cmr_roads_pgsqlAPI.php'
+    ?>
+    <script>
+        var format = 'image/png';
+        var map;
+        var minX = 8.8262996673584;
+        var minY = 1.97410714626312;
+        var maxX = 15.5927391052246;
+        var maxY = 12.7209100723267;
+        var cenX = (minX + maxX) / 2;
+        var cenY = (minY + maxY) / 2;
+        var mapLat = cenY;
+        var mapLng = cenX;
+        var mapDefaultZoom = 7;
+        var container = document.getElementById('popup');
+        var content = document.getElementById('popup-content');
+        var closer = document.getElementById('popup-closer');
+        var overlay = new ol.Overlay({
+            element: container,
+            autoPan: true,
+        });
+        
+        function reset(){
+            document.getElementById("LoaderBalls__item").style.display = "block";
+            document.getElementById("LoaderBalls__item1").style.display = "block";
+            document.getElementById("LoaderBalls__item2").style.display = "block";
+             document.getElementById("broad-content").innerHTML="";
+        }
+        var cmr_adm1 = new ol.layer.Group({
+            layers: [ new ol.layer.Image({
+             source: new ol.source.ImageWMS({
+                    ratio: 1,
+                    url: 'http://localhost:8080/geoserver/example/wms?',
+                    params: {
+                        'FORMAT': format,
+                        'VERSION': '1.1.1',
+                        STYLES: '',
+                        LAYERS: 'cmr_adm1',
+                    }
+                }),
+            })]
+        });
+        var layersOSM = new ol.layer.Group({
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ]
+        });
+        function initinizeMap(){
+            closer.onclick = function() {
+                overlay.setPosition(undefined);
+                closer.blur();
+                return false;
+            };
+            var viewMap = new ol.View({
                     center: ol.proj.fromLonLat([mapLng, mapLat]),
                     zoom: mapDefaultZoom
-                    //projection: projection
                 });
-                map = new ol.Map({
-                    overlays: [overlay],
-                    target: "map",
-                    layers: [
-                        layersOSM,cmr_roads
-                    ],
-                    overlays: [overlay],
-                    //layers: [layerDuongDiaGioi],
-                    view: viewMap
-                });
-                
-                //map.getView().fit(bounds, map.getSize());
-                map.addOverlay(overlay);
-                var styles = {
-                    'MultiLineString': new ol.style.Style({
-                        stroke: new ol.style.Stroke({
-                            color: 'yellow', 
-                            width: 3
-                        })
-                    }),
-                    'Polygon': new ol.style.Style({
-                        stroke: new ol.style.Stroke({
-                            color: 'yellow', 
-                            width: 3
-                        })
+            map = new ol.Map({
+                overlays: [overlay],
+                target: "map",
+                layers: [
+                    layersOSM,cmr_adm1
+                ],
+                overlays: [overlay],
+                view: viewMap
+            });
+            map.addOverlay(overlay);
+            var styles = {
+                'MultiLineString': new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: '#37474f', 
+                        width: 3
                     })
-                };
-                var styleFunction = function (feature) {
-                    return styles[feature.getGeometry().getType()];
-                };
-                var vectorLayer = new ol.layer.Vector({
-                    //source: vectorSource,
-                    style: styleFunction
-                });
-                map.addLayer(vectorLayer);
-
-                function createJsonObj(result) {                    
+                }),
+                'Polygon': new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: '#37474f', 
+                         width: 3
+                    })
+                })
+            };
+            var styleFunction = function (feature) {
+                return styles[feature.getGeometry().getType()];
+            };
+            var vectorLayer = new ol.layer.Vector({
+                style: styleFunction
+            });
+            map.addLayer(vectorLayer);
+            function createJsonObj(result) {                    
                     var geojsonObject = '{'
                             + '"type": "FeatureCollection",'
                             + '"crs": {'
@@ -275,14 +247,13 @@
                             + '}]'
                         + '}';
                     return geojsonObject;
-                }
-                
-                function displayObjInfo(result,coordinate )
+            }
+            function displayObjInfo(result,coordinate )
                 {
-                    content.innerHTML = result 
+                    content.innerHTML = result;
                     overlay.setPosition(coordinate);
                 }
-                function displayObjInfoB(result,coordinate )
+            function displayObjInfoBroad(result,coordinate )
                 {
                     document.getElementById("LoaderBalls__item").style.display = "none";
                     document.getElementById("LoaderBalls__item1").style.display = "none";
@@ -291,7 +262,7 @@
                     document.getElementById("broad-content").innerHTML=result;
                 }
                 
-                function highLightGeoJsonObj(paObjJson) {
+            function highLightGeoJsonObj(paObjJson) {
                     var vectorSource = new ol.source.Vector({
                         features: (new ol.format.GeoJSON()).readFeatures(paObjJson, {
                             dataProjection: 'EPSG:4326',
@@ -299,29 +270,22 @@
                         })
                     });
 					vectorLayer.setSource(vectorSource);
-                }
-                function highLightObj(result) {
-                    //alert("result: " + result);
+            }
+            function highLightObj(result) {
                     var strObjJson = createJsonObj(result);
-                    //alert("Full Json: " + strObjJson);
                     var objJson = JSON.parse(strObjJson);
-                    //alert("Full Json: " + JSON.stringify(objJson));
-                    //drawGeoJsonObj(objJson);
                     highLightGeoJsonObj(objJson);
                 }
-                map.on('singleclick', function (evt) {
-                    //alert("coordinate: " + evt.coordinate);
-                    //var myPoint = 'POINT(12,5)';
+            map.on('singleclick', function (evt) {
                     var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
                     var lon = lonlat[0];
                     var lat = lonlat[1];
                     var myPoint = 'POINT(' + lon + ' ' + lat + ')';
-                    //alert("myPoint: " + myPoint);
-                    //*
+                    //Tô màu
                     $.ajax({
                         type: "POST",
                         url: "cmr_roads_pgsqlAPI.php",
-                        //dataType: 'json',
+                        dataType:'json',
                         data: {functionname: 'getGeoCMRRoadsToAjax', paPoint: myPoint},
                         success : function (result, status, erro) {
                             highLightObj(result);
@@ -329,14 +293,13 @@
                         error: function (req, status, error) {
                             alert(req + " " + status + " " + error);
                         }
-                        
                     });
-                    //*/
+                    //Hiện popup về diện tích ,chu vi....
                     $.ajax({
                         type: "POST",
                         url: "cmr_roads_pgsqlAPI.php",
-                        //dataType: 'json',
-                        data: {functionname: 'getInfoCMRRoadsToAjax', paPoint: myPoint},
+                        dataType:'json',
+                        data: {functionname: 'getCaculatorToAjax', paPoint: myPoint},
                         success : function (result, status, erro) {
                             displayObjInfo(result,evt.coordinate);
                         },
@@ -344,13 +307,14 @@
                             alert(req + " " + status + " " + error);
                         }
                     });
+                    //Hiện info của vùng ra broad
                     $.ajax({
                         type: "POST",
                         url: "cmr_roads_pgsqlAPI.php",
-                        //dataType: 'json',
-                        data: {functionname: 'getinfor', paPoint: myPoint},
+                        dataType:'json',
+                        data: {functionname: 'getInfoToAjax', paPoint: myPoint},
                         success : function (result, status, erro) {
-                            displayObjInfoB(result,evt.coordinate);
+                            displayObjInfoBroad(result,evt.coordinate);
                         },
                         error: function (req, status, error) {
                             alert(req + " " + status + " " + error);
@@ -359,27 +323,19 @@
                     });
                 });      
             };
-            $("#cmr_roads").change(function () {
-                if($("#cmr_roads").is(":checked"))
-                {
-                    cmr_roads.setVisible(true);
-                }
+            $("#cmr_adm1").change(function () {
+                if($("#cmr_adm1").is(":checked"))
+                    cmr_adm1.setVisible(true);
                 else
-                {
-                    cmr_roads.setVisible(false);
-                }
+                    cmr_adm1.setVisible(false);
             });
             $("#layersOSM").change(function () {
                 if($("#layersOSM").is(":checked"))
-                {
                     layersOSM.setVisible(true);
-                }
                 else
-                {
                     layersOSM.setVisible(false);
-                }
             });
-            
-        </script>
-    </body>
+    </script>
+</body>
+
 </html>
