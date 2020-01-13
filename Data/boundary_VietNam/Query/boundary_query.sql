33eb41770ab5c88a86ee1bf5c5d256459a4f1ee5
DaiLuu-bit <jojolove198@gmail.com>
2020-01-13 10:34:36 +0700

    Ham chuyen tu latin1 sang utf8

---
+++ Data/boundary_VietNam/Query/boundary_query.sql
@@ -0,0 +1,16 @@
+﻿create function latin1ToUtf8 ( str text )
+	returns text as $$
+declare 
+res text;
+begin
+	res := 
+	convert_from(
+	convert(
+	(select convert_to(str, 'UTF-8'))
+	, 'utf-8', 'latin-1')
+	, 'utf-8');
+	return res;
+end; $$
+language plpgsql;
+
+select latin1ToUtf8('Viá»t Nam')
